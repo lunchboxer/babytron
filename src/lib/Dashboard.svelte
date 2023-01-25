5f47f9329ac6639e7646bd39ba19afb3f632fe02
James <metal.lunchbox@gmail.com>
2023-01-25 21:55:16 +0800

    simple mock-up of some stats for dashboard

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -1,10 +1,13 @@
 <script>
   import { selectedBaby } from '$lib/data/babies.js'
   import RecordMeasurement from '$lib/RecordMeasurement.svelte'
+  import Stats from '$lib/Stats.svelte'
   import BabyName from '$lib/BabyName.svelte'
 </script>
 
 <h1>Dashboard</h1>
 <h2><BabyName baby={$selectedBaby} /></h2>
+
+<Stats baby={$selectedBaby} />
 
 <RecordMeasurement baby={$selectedBaby} />
---
+++ src/lib/Stats.svelte
@@ -0,0 +1,46 @@
+<script>
+  import Fa from 'svelte-fa'
+  import {
+    faPoo,
+    faWeightScale,
+    faRuler,
+    faPersonBreastfeeding,
+  } from '@fortawesome/free-solid-svg-icons'
+  export let baby = {}
+</script>
+
+<div class="stats stats-vertical lg:stats-horizontal shadow">
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <Fa icon={faPoo} size="2x" />
+    </div>
+    <div class="stat-title">Diapers</div>
+    <div class="stat-value">687</div>
+    <div class="stat-desc">Average 6.8/day</div>
+  </div>
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <Fa icon={faWeightScale} size="2x" />
+    </div>
+    <div class="stat-title">Weight (kg)</div>
+    <div class="stat-value">6.2</div>
+    <div class="stat-desc">↗︎ ~150g/week</div>
+  </div>
+
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <Fa icon={faPersonBreastfeeding} size="2x" />
+    </div>
+    <div class="stat-title">Last fed</div>
+    <div class="stat-value">45 min</div>
+    <div class="stat-desc">212 minutes today</div>
+  </div>
+  <div class="stat">
+    <div class="stat-figure text-secondary">
+      <Fa icon={faRuler} size="2x" />
+    </div>
+    <div class="stat-title">Length (cm)</div>
+    <div class="stat-value">75.8</div>
+    <div class="stat-desc">↗︎︎ ~2.5 cm/month</div>
+  </div>
+</div>
